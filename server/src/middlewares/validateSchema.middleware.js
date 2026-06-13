a14e7bc7176466f6e83a7faf52645ce4c3ef4e2f
Yuvraj Mehta <yuvraj.mehta532@gmail.com>
2026-06-13 15:37:45 +0530

    fix(middleware): replace createRequire shim with ESM JSON import assertion

--- server/src/middlewares/validateSchema.middleware.js
+++ server/src/middlewares/validateSchema.middleware.js
@@ -1,9 +1,6 @@
 import Ajv from "ajv";
 import addFormats from "ajv-formats";
-import { createRequire } from "module";
-
-const require = createRequire(import.meta.url);
-const portfolioSchema = require("../schema/portfolio.schema.json");
+import portfolioSchema from "../schema/portfolio.schema.json" assert { type: "json" };
 
 const ajv = new Ajv({ allErrors: true, coerceTypes: true });
 addFormats(ajv);
